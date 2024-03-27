37a381089f9bfb064ea0edead64703c4fb24bc75
goofy foot <williamk31@gmail.com>
2024-03-26 18:24:42 -0600

    fixed tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,11 +23,12 @@ router.get('/:id', async (req, res) => {
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product, through: ProductTag, as: 'tag_product' }]
     });
-
+   
     if (!tagData) {
       res.status(404).json({ message: 'No tag with that id!'});
     }
-
+    
+    res.status(200).json(tagData)
   } catch (err) {
     res.status(500).json(err);
   }
